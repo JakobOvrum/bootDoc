65f9eb3410d2ae0717bb2d81b681ab8b76e178f8
Jakob Ovrum <jakobovrum@gmail.com>
2012-05-09 00:32:38 +0900

    Fix default settings.ddoc to work with example/example.d and expand the example with some more tests

--- example/example.d
+++ example/example.d
@@ -1,4 +1,37 @@
 module example.example;
 
-///
-void foo() {}
+/// Ut in enim magna, et vestibulum dui. Phasellus commodo ullamcorper.
+void foo();
+
+/**
+ * Cras aliquet auctor dictum. Vestibulum posuere dolor sit amet arcu.
+ * Params:
+ *    a = Vivamus vitae semper eros. Cras.
+ *    b = Sed pellentesque orci id felis.
+ */
+int bar(int a, int b);
+
+/// Nulla eu eros et neque aliquam fermentum dictum eu turpis.
+struct S
+{
+	/**
+	 * Params:
+	 *    a = Donec massa augue, mattis id.
+	 */
+	this(int a);
+	
+	///
+	immutable this(int a);
+	
+	///
+	@disable this();
+	
+	///
+	~this();
+	
+	///
+	this(this);
+	
+	/// Etiam gravida odio sed massa.
+	static S foo();
+}
